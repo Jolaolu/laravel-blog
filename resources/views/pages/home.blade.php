dd950155c330b1817503a6fed3c9827cf48e87ae
jolaolu <adebayoanjola@gmail.com>
2018-10-16 20:33:48 +0100

    CRUD implemented fullygit add .

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -3,8 +3,18 @@
 
 @section('content')
     <div class="container">
-        <h2> Dashboard</h2><hr>
-        <span class=""><a href="/posts/create" class="btn btn-secondary" role="button">Create New Post</a></span>
+        <div class="row">
+            <div class="col-md-8 col-md-offset-2">
+                <div class="panel-heading">
+                    <h2> Dashboard</h2><hr>
+                </div>
+                <div class="panel-body">
+                    <span class=""><a href="/posts/create" class="btn btn-secondary" role="button">Create New Post</a></span>
+                    <h3>Your Blog Posts</h3>
+                </div>
+            </div>
+        </div>
     </div>
+
 
 @endsection
