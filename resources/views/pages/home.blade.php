ead753dfc7bfdcf39400cdb74d39103a5b4c8eb5
jolaolu <adebayoanjola@gmail.com>
2018-10-16 20:37:16 +0100

    CRUD implemented fully

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -5,12 +5,14 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 col-md-offset-2">
-                <div class="panel-heading">
-                    <h2> Dashboard</h2><hr>
-                </div>
-                <div class="panel-body">
-                    <span class=""><a href="/posts/create" class="btn btn-secondary" role="button">Create New Post</a></span>
-                    <h3>Your Blog Posts</h3>
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h2> Dashboard</h2><hr>
+                    </div>
+                    <div class="panel-body">
+                       <a href="/posts/create" class="btn btn-secondary" role="button">Create New Post</a>
+                        <h3>Your Blog Posts</h3>
+                    </div>
                 </div>
             </div>
         </div>
